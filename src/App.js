88218a33b046bca697f20ae0c5a995cc9d674fd0
Leandro Hosken <lshv04@gmail.com>
2024-10-29 11:55:29 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Geocoding from './pages/Geocoding';
 import Home from './pages/Home';
 import Detail from './pages/Detail'; // Importando a página Detail
 import Extra from './pages/Extra'; // Importando a página Extra
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbarone from './components/Navbar';
 import Footer from './components/Footer';
 
