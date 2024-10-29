99edbddae32b1b1c2c3e3ae04b54c64f836ee2d1
Leandro Hosken <lshv04@gmail.com>
2024-10-29 09:57:50 -0300

    estilizando geocoding

--- src/pages/Geocoding.js
+++ src/pages/Geocoding.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useFetch from '../hooks/useFetch';
 import { useSearchParams, Link } from 'react-router-dom';
+import './Geocoding.css'; // Importando o CSS
 
 const options = {};
 
@@ -9,41 +10,38 @@ const Geocoding = () => {
   const query = searchParams.get('query'); // Captura o parâmetro 'query' da URL
 
   const { data, loading, error } = useFetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=797703c9bbce7164cfab34943034bf2b`,
-    options
+    `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=797703c9bbce7164cfab34943034bf2b`, options
   );
 
   if (loading) {
-    return <h2>Carregando...</h2>; // Exibe uma mensagem de carregamento
+    return <h2>Loading...</h2>;
   }
 
   if (error) {
-    return <h2>Erro: {error.message}</h2>; // Exibe uma mensagem de erro, se houver
+    return <h2>Error: {error.message}</h2>;
   }
-
-  if (data && data.length === 0) {
-    return <h2>Nenhum resultado encontrado para "{query}".</h2>; // Mensagem para array vazio
-  }
-
-  const formatName = (name) => {
-    return name.replace(/\s+/g, '-').toLowerCase(); // Substitui espaços por hífens e converte para minúsculas
-  };
 
   return (
-    <div>
-      <h1>Geocoding Resultados</h1>
-
-      {data && (
-        <ul>
+    <div className="geocoding-container">
+      <h1 className="geocoding-title">Search Results for "{query}"</h1>
+      {data && data.length > 0 ? (
+        <div className="row geocoding-grid">
           {data.map((item, index) => (
-            <li key={index}>
-              {item.name}, {item.country} / {item.state} - Latitude: {item.lat}, Longitude: {item.lon}
-              <Link to={`/detail/${formatName(item.name)}/${item.lat}/${item.lon}`}>
-                Ver detalhes
-              </Link>
-            </li>
+            <div key={index} className="col-12 col-md-6 mb-4">
+              <div className="geocoding-item p-3">
+                <h2>{item.name}, {item.country}</h2>
+                <p>State: {item.state}</p>
+                <p>Latitude: {item.lat}, Longitude: {item.lon}</p>
+                {/* Botão Bootstrap para redirecionar para a página Detail */}
+                <Link to={`/detail/${item.name}/${item.lat}/${item.lon}`} className="btn btn-primary">
+                  View Details
+                </Link>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
+      ) : (
+        <p className="geocoding-no-results">No results found.</p>
       )}
     </div>
   );
